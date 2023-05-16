eaa45291c60eef985888680f02515f7131e8712b
minyoung <aroem0825@naver.com>
2023-05-16 16:01:08 +0900

    modal: props수정

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
+<template>
+  <div>
+    <h1>ts-vue 로 만드는 KanbanBoard</h1>
+    {{ selectedValue }}
+    텍스트 입력<input v-model="inputValue" @keyup.enter="addItem" />
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+
+        {{ item }}
+        <button type="button" @click="clickValue = index; selectedValue = item; modal = true;">자세한 일정 추가하기</button>
+      </li>
+    </ul>
+    <Modal v-if="modal" :modal="modal" :item="selectedValue" :clickValue="clickValue" @closeModal="modal = false"/>
+  </div>
+</template>
+
+
 <script setup lang="ts">
-import { ref} from 'vue'
+import { ref } from 'vue'
 import Modal from './components/modal.vue'
 
 const inputValue = ref<string>('');
 const items = ref<string[]>([]);
+const modal = ref<Boolean>(false);
+const clickValue = ref<Number>(0); // 클릭한값
+const selectedValue = ref<string>('')
+
+
 
 const addItem = () => {
   items.value.push(inputValue.value)
   inputValue.value = '' // 입력 필드를 초기화
 }
+const handleSelect = (selectedOption: string) => {
+
+}
 </script>
-
-<template>
-  <div>
-    <h1>ts-vue 로 만드는 KanbanBoard</h1>
-
-    텍스트 입력<input v-model="inputValue" @keyup.enter="addItem" />
-    <ul>
-      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
-    </ul>
-    <Modal/>
-  </div>
-</template>
-
 
 
 <style scoped></style>
--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,14 +1,72 @@
 <template>
-  <div>
-    <h1>modal</h1>
-    <div class="addList"></div>
+  <div class="modal" v-if="modal">
+    <div class="modalContainer">
+      <h1>modal</h1>
+      <div class="addList">
+        {{ clickValue }}
+        {{ item }}
+        <button type="button" @click.stop="$emit('closeModal')" class="closeButton">X</button>
+      </div>
+      <div class="checkBox">
+        <input v-model="checkInput" @keyup.enter="addCheckbox" />
+        <div>
+          {{ selectOption }}
+          <select v-model="selectOption">
+            <option>Todo</option>
+            <option>Doing</option>
+            <option>Done</option>
+          </select>
+        </div>
+        <ul>
+          <li v-for="(item, index) in checkList" :key="index">{{ item }}</li>
+        </ul>
+        <button type="button" @click.stop="$emit('select', selectOption)">button</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
+
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const checkList = ref<String[]>([])
+const checkInput = ref<String>('')
+const selectOption = ref<String>('')
+
+const addCheckbox = () => {
+  checkList.value.push(checkInput.value)
+  checkInput.value = '' // 입력 필드를 초기화
+}
+
+const props = defineProps<{
+  modal: Boolean,
+  item: string,
+  clickValue: Number,
+}>()
 
 </script>
 
 <style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.685);
+}
 
+.modalContainer {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate3d(-50%, -50%, 0);
+  width: 500px;
+  height: 440px;
+  background-color: #fff;
+  border-radius: 20px;
+
+}
 </style>
