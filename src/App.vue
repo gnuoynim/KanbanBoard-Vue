6ebde76c8e15226a9a61ab669854e166e8b716f6
minyoung <aroem0825@naver.com>
2023-05-16 18:03:41 +0900

    props수정

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
     <h1>ts-vue 로 만드는 KanbanBoard</h1>
-    {{ selectedValue }}
     텍스트 입력<input v-model="inputValue" @keyup.enter="addItem" />
     <ul>
       <li v-for="(item, index) in items" :key="index">
-
-        {{ item }}
-        <button type="button" @click="clickValue = index; selectedValue = item; modal = true;">자세한 일정 추가하기</button>
+        {{ item.value }}
+        {{ item.id }}
+        <button type="button" @click="clickValue = index; inputValue = item.value; modal = true;">자세한 일정 추가하기</button>
       </li>
     </ul>
-    <Modal v-if="modal" :modal="modal" :item="selectedValue" :clickValue="clickValue" @closeModal="modal = false"/>
+    <Modal v-if="modal" :modal="modal" :item="inputValue" :clickValue="clickValue" @closeModal="modal = false" @select="selectOption"/>
   </div>
 </template>
 
@@ -20,20 +19,17 @@ import { ref } from 'vue'
 import Modal from './components/modal.vue'
 
 const inputValue = ref<string>('');
-const items = ref<string[]>([]);
+const items = ref<Array<{ id: number; value: string }>>([]);
 const modal = ref<Boolean>(false);
 const clickValue = ref<Number>(0); // 클릭한값
-const selectedValue = ref<string>('')
-
-
+const selection = ref<String>('')
 
 const addItem = () => {
-  items.value.push(inputValue.value)
-  inputValue.value = '' // 입력 필드를 초기화
+  items.value.push({id:Date.now(), value:inputValue.value})
+  inputValue.value = '' 
 }
-const handleSelect = (selectedOption: string) => {
 
-}
+
 </script>
 
 
